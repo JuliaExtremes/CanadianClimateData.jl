ecd108e14070debfdc22ca0084686fd893cf24d7
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2025-07-11 11:13:47 -0400

    add download idf zip and unzip functions

---
+++ src/idf.jl
@@ -0,0 +1,101 @@
+file_version = "idf_v3-30_2022_10_31"
+
+repo_url = "https://collaboration.cmc.ec.gc.ca/cmc/climate/Engineer_Climate/IDF/"*file_version*"/IDF_Files_Fichiers/"
+
+prov_list = ["AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"]
+
+"""
+    idf_zip_download(province::String ; dir::String="..")
+
+Download the ZIP files of the IDF of the province `province` in `dir`.
+"""
+function idf_zip_download(province::String ; dir::String=pwd())
+    @assert province ∈ prov_list "The provided province $province is not valid."
+
+    repo_file_name = "$(province).zip"
+
+    repo_file_url = joinpath(dirname(@__FILE__), repo_url, repo_file_name)
+
+    zip_path = joinpath(dirname(@__FILE__), dir, repo_file_name)
+
+    Downloads.download(repo_file_url, zip_path)
+
+    return zip_path
+
+end
+
+"""
+    idf_unzip(zip_path::String)
+
+Unzip only the text files contained in the archive.
+
+See also: [`idf_zip_download`](@ref)
+"""
+function idf_unzip(zip_path::String)
+
+    output_dir = splitext(zip_path)[1]
+
+    run(`unzip -j $zip_path '*.txt' -d $output_dir`)
+end
+
+
+
+
+#     for province in provinces
+#         # Make a temp directory for all data :
+#         try
+#             cd("$(output_dir)/temp_data")
+#         catch
+#             mkdir("$(output_dir)/temp_data")
+#             cd("$(output_dir)/temp_data")
+#         end
+
+#         file = "$(province).zip"
+
+#         # Download the data (if not downloaded already) and unzip the data :
+#         if file in glob("*", pwd())
+#             run(`unzip $(file) "*.txt"`)   # unzip the data
+#         else
+#             Downloads.download("$(url)$(file)", "$(file)")
+#             try
+#                 run(`unzip $(file) "*.txt"`)   # unzip the data
+#                 cd("$(output_dir)")
+#             catch
+#                 throw(error("Unable to unzip the data file."))
+#             end
+#         end
+
+#         input_d = "$(output_dir)/temp_data/$(province)" # Where raw data are
+#         if split
+#             # Make the output directory if it doesn't exist :
+#             try
+#                 mkdir("$(output_dir)/$(province)")
+#             catch
+#                 nothing
+#             end
+#             output_d = "$(output_dir)/$(province)" # Where the netcdf/csv will be created
+#         else
+#             output_d = "$(output_dir)/"
+#         end
+
+#         # Convert the data in the specified format (CSV or NetCDF) :
+#         if lowercase(format) == "csv"
+#             #txt2csv(input_d, output_d, province)
+#             info_df = vcat(info_df, txt2csv(input_d, output_d, province))
+#         elseif lowercase(format) == "netcdf" || lowercase(format) == "nc"
+#             txt2netcdf(input_d, output_d)
+#         else
+#             throw(error("Format is not valid"))
+#         end
+
+#         # Automatic deletion
+#         if rm_temp
+#             rm("$(output_dir)/temp_data", recursive=true)
+#         end
+#     end
+#     if lowercase(format) == "csv"
+#         output_info = "$(output_dir)/info_stations.csv"
+#         CSV.write(output_info, info_df)
+#     end
+#     return nothing
+# end
---
+++ test/idf_test.jl
@@ -0,0 +1,15 @@
+@testset "idf_zip_download" begin
+    @test_throws AssertionError CanadianClimateData.idf_zip_download("nonexistant")
+    @test isfile(CanadianClimateData.idf_zip_download("PE", dir=mktempdir()))
+end
+
+@testset "idf_unzip" begin
+    zip_path = CanadianClimateData.idf_zip_download("PE", dir=mktempdir())
+
+    idf_unzip(zip_path)
+
+    parent_directory = splitext(zip_path)[1]
+    txt_file_name = joinpath(dirname(@__FILE__), parent_directory, "idf_v3-30_2022_10_31_830_PE_8300516_NORTH_CAPE.txt")
+
+    @test isfile(txt_file_name)
+end
